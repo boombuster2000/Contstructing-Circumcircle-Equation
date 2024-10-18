772c7f9e93f35b8021e7d6dde5e353dc5b291ac5
boombuster2000 <chanuka.williams@outlook.com>
2024-10-18 17:59:35 +0100

    Add project files.

---
+++ Contstructing Circumcircle Equation/Contstructing Circumcircle Equation.cpp
@@ -0,0 +1,12 @@
+﻿// Contstructing Circumcircle Equation.cpp : Defines the entry point for the application.
+//
+
+#include "Contstructing Circumcircle Equation.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
